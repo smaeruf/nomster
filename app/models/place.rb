702a4a41a2dcbb7dc5b25f53fa29c27bb4352e85
smaeruf <smaeruf@comcast.net>
2019-09-08 19:54:50 +0000

    setting up the maps integration- latitude and longitude, using geocoding api

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
   validates :name, :address, :description, presence: true
   validates :name, length: { minimum: 4 }, presence: true
 end
