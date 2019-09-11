ee0be44087a32e27b21098ee4eb6abf41f47f77f
smaeruf <smaeruf@comcast.net>
2019-09-11 23:34:09 +0000

    adding image upload

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,5 @@ class Photo < ApplicationRecord
   belongs_to :place
   belongs_to :user
   mount_uploader :picture, PictureUploader
+  add column :string
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_152304) do
+ActiveRecord::Schema.define(version: 2019_09_11_232911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_09_10_152304) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["place_id", "user_id"], name: "index_photos_on_place_id_and_user_id"
     t.index ["place_id"], name: "index_photos_on_place_id"
   end
