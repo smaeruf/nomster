432b27bb218fef145ef49e7ead567aa034ef9cf1
smaeruf <smaeruf@comcast.net>
2019-09-11 23:09:22 +0000

    adding image upload

--- db/migrate/20190910152304_create_photos.rb
+++ db/migrate/20190910152304_create_photos.rb
@@ -1,7 +1,7 @@
 class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
-      t.text :capiton
+      t.text :caption
       t.integer :place_id
       t.integer :user_id
       t.timestamps
